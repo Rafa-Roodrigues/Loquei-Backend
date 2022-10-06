f54141f526059cec04d7d4dfe4db875cd48c49b0
Rafael Rodrigues <rodriguesrafael446@gmail.com>
2022-10-06 17:22:13 -0300

    testee

--- dist/config/token.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configToken = void 0;
-exports.configToken = {
-    secret: '633d33c618681142ad4f795e7e528469'
-};
