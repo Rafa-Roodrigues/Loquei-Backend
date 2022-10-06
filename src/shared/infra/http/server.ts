df02a577087e0b1904a8169311bb5f0bbaeb979c
Rafael Rodrigues <rodriguesrafael446@gmail.com>
2022-10-06 12:59:22 -0300

    Cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -12,13 +12,15 @@ import cors from 'cors';
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, DELETE, POST, PATCH");
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, DELETE, POST, PATCH");
 
-  app.use(cors());
-  next();
-})
+//   app.use(cors());
+//   next();
+// })
+
+app.use(cors());
 
 app.use(express.json());
 
